a40535444b1ad504fcf9ec681e597508bc8885cb
Mayank Kamra <mayank22kamra@gmail.com>
2020-08-21 18:21:24 +1000

    Initial Commit

---
+++ FIT5140-Week03-Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT5140-Week03-Lab
+//
+//  Created by Mayank Kamra on 21/8/20.
+//  Copyright © 2020 Mayank Kamra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
