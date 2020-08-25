081c96caf14adf033b344772f2822f592431a2ab
Mayank Kamra <mayank22kamra@gmail.com>
2020-08-25 16:25:28 +1000

    initial build completed

---
+++ FIT5140-Week03-Lab/AddSuperHeroDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AddSuperHeroDelegate.swift
+//  FIT5140-Week03-Lab
+//
+//  Created by Mayank Kamra on 21/8/20.
+//  Copyright © 2020 Mayank Kamra. All rights reserved.
+//
+
+import Foundation
---
+++ FIT5140-Week03-Lab/SuperHeroTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SuperHeroTableViewCell.swift
+//  FIT5140-Week03-Lab
+//
+//  Created by Mayank Kamra on 21/8/20.
+//  Copyright © 2020 Mayank Kamra. All rights reserved.
+//
+
+import UIKit
+
+class SuperHeroTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
